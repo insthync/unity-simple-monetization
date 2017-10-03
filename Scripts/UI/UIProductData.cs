3edd0e08a328417f2fa7a2c286ca987fee4619e5
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-10-04 04:28:40 +0700

    Add UI for product purchase

---
+++ Scripts/UI/UIProductData.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIProductData : MonoBehaviour
+{
+    [Header("UI")]
+    public Text textTitle;
+    public Text textDescription;
+    public Text textPrice;
+    public RawImage iconImage;
+    public RawImage previewImage;
+    [Header("Product Data")]
+    public BaseProductData productData;
+    private BaseProductData dirtyProductData;
+
+    private void Update()
+    {
+        if (productData != dirtyProductData)
+        {
+            UpdateData();
+            dirtyProductData = productData;
+        }
+    }
+
+    public void UpdateData()
+    {
+        var title = "";
+        var description = "";
+        var price = "";
+        Texture iconTexture = null;
+        Texture previewTexture = null;
+        if (productData != null)
+        {
+            title = productData.GetTitle();
+            description = productData.GetDescription();
+            price = productData.GetPriceText();
+            iconTexture = productData.iconTexture;
+            previewTexture = productData.previewTexture;
+        }
+        if (textTitle != null)
+            textTitle.text = title;
+        if (textDescription != null)
+            textDescription.text = description;
+        if (textPrice != null)
+            textPrice.text = price;
+        if (iconImage != null)
+            iconImage.texture = iconTexture;
+        if (previewImage != null)
+            previewImage.texture = previewTexture;
+    }
+}
---
+++ Scripts/UI/UIProductList.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIProductList : MonoBehaviour
+{
+    public BaseProductData[] products;
+    public UIProductData prefab;
+    public Transform container;
+    private readonly Dictionary<string, UIProductData> UIs = new Dictionary<string, UIProductData>();
+
+    private void Awake()
+    {
+        foreach (var product in products)
+        {
+            AddProduct(product);
+        }
+    }
+
+    public void AddProduct(BaseProductData productData)
+    {
+        if (productData == null || UIs.ContainsKey(productData.GetId()))
+            return;
+        var uiObject = Instantiate(prefab.gameObject);
+        uiObject.SetActive(true);
+        uiObject.transform.SetParent(container, false);
+        var ui = uiObject.GetComponent<UIProductData>();
+        ui.productData = productData;
+        UIs[productData.GetId()] = ui;
+    }
+
+    public bool RemoveProduct(string id)
+    {
+        if (string.IsNullOrEmpty(id) || !UIs.ContainsKey(id))
+            return false;
+        var ui = UIs[id];
+        if (UIs.Remove(id))
+        {
+            Destroy(ui.gameObject);
+            return true;
+        }
+        return false;
+    }
+
+    public void ClearProducts()
+    {
+        UIs.Clear();
+        foreach (var child in container)
+        {
+            Destroy(container.gameObject);
+        }
+    }
+}
