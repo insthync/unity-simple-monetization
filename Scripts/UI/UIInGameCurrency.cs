6491ce2c78797c614becb4d822be73a6743e1b06
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-10-08 15:19:11 +0700

    Add UI for currency

---
+++ Scripts/UI/UIInGameCurrency.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIInGameCurrency : MonoBehaviour
+{
+    public Text textCurrencyAmount;
+    public InGameCurrency currency;
+    [Tooltip("Format {0} = amount, {1} = name, {2} = symbol")]
+    public string displayFormat = "{1} {0} {2}";
+
+    private void Update()
+    {
+        if (MonetizationManager.Currencies.ContainsKey(currency.id))
+        {
+            var setting = MonetizationManager.Currencies[currency.id];
+            textCurrencyAmount.text = string.Format(displayFormat, currency.amount.ToString("N0"), setting.name, setting.symbol);
+        }
+        else
+            textCurrencyAmount.text = "N/A";
+    }
+}
---
+++ Scripts/UI/UISavedInGameCurrency.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(UIInGameCurrency))]
+public class UISavedInGameCurrency : MonoBehaviour {
+    public string currencyId;
+    private UIInGameCurrency uiInGameCurrency;
+    private void Awake()
+    {
+        uiInGameCurrency = GetComponent<UIInGameCurrency>();
+    }
+
+    private void Update()
+    {
+        var amount = MonetizationSave.GetCurrency(currencyId);
+        uiInGameCurrency.currency.id = currencyId;
+        uiInGameCurrency.currency.amount = amount;
+    }
+}
