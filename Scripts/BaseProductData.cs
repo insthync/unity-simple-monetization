11310e8b18b9c3e1a60e50349fad98e1b599fe4b
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-10-13 16:36:35 +0700

    Remove category, I have no idea what I will do with it.

--- Scripts/BaseProductData.cs
+++ Scripts/BaseProductData.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public abstract class BaseProductData : ScriptableObject
 {
-    public string category;
     public Texture iconTexture;
     public Texture previewTexture;
     public abstract string GetId();
