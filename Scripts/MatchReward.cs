3fed45228f396d676b9cfb78b561a604dc9ac752
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-11-09 13:07:20 +0700

    Add match reward, will use it to random rewards

---
+++ Scripts/MatchReward.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public struct MatchReward
+{
+    public AdsReward[] rewards;
+
+    public AdsReward RandomReward()
+    {
+        var cacheRewards = new Dictionary<AdsReward, int>();
+        foreach (var reward in rewards)
+        {
+            cacheRewards[reward] = reward.randomWeight;
+        }
+        var randomizer = WeightedRandomizer.From(cacheRewards);
+        return randomizer.TakeOne();
+    }
+}
