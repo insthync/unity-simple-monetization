08ef35cdf9a1b2df2e384662c3e59bab8e647622
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-09-06 00:56:08 +0700

    Add helper component to show/hide UI objects while player already bought product

---
+++ Scripts/UI/UIProductOwningActivatator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIProductOwningActivatator : MonoBehaviour
+{
+    public UIInGameProductData uiProductData;
+    public GameObject[] activatingWhileOwningObjects;
+    public GameObject[] activatingWhileNotOwningObjects;
+
+    private void Update()
+    {
+        if (uiProductData == null || uiProductData.productData == null)
+            return;
+
+        var inGameProductData = uiProductData.productData as InGameProductData;
+        foreach (var obj in activatingWhileOwningObjects)
+        {
+            obj.SetActive(inGameProductData.IsBought());
+        }
+        foreach (var obj in activatingWhileNotOwningObjects)
+        {
+            obj.SetActive(!inGameProductData.IsBought());
+        }
+    }
+}
